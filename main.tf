5c68eefd127c2a382974c1cb7eb6ab30025b496a
shalaermal <ermalshala10@gmail.com>
2026-02-28 08:39:25 -0500

    Adding IAM role. Adding Lambda and testing it

--- main.tf
+++ main.tf
@@ -8,3 +8,36 @@ resource "aws_dynamodb_table" "tasks" {
     type = "S"
   }
 }
+
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda-basic-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+#Attach logging policy
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+#Lambda Function
+resource "aws_lambda_function" "basic_lambda" {
+  function_name = "basic-terraform-lambda"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "app.lambda_handler"
+  runtime       = "python3.9"
+
+  filename         = "lambda/function.zip"
+  source_code_hash = filebase64sha256("lambda/function.zip")
+}
+
